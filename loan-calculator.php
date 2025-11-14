ac92d5734874c853709ce7c5cbf31a73dfc63d71
mstrgc <gcmstr@gmail.com>
2025-11-14 15:57:03 +0330

    add new path

--- loan-calculator.php
+++ loan-calculator.php
@@ -10,6 +10,7 @@ if(!defined('ABSPATH')) {
 
 define('LCPLUGIN_PATH', plugin_dir_path(__FILE__));
 define('LCPLUGIN_INCLUDES', LCPLUGIN_PATH . 'includes/');
+define('LCPLUGIN_CONFIG', LCPLUGIN_PATH . 'config/');
 
 require_once LCPLUGIN_INCLUDES . 'class-loan-shortcode.php';
 
