9f8b2196cc028b5b352937b6e56da8471ac6a67a
mstrgc <gcmstr@gmail.com>
2025-11-14 14:38:23 +0330

    add input methods

--- includes/class-base-config.php
+++ includes/class-base-config.php
@@ -3,3 +3,25 @@
 if(!defined('ABSPATH')) {
   exit;
 }
+
+class Loan_Calc_Base_Config{
+  public function __construct(){}
+
+  public $inputs = [];
+
+  public function add_input($attrs){
+    $this->inputs[] = [
+      'type' => $attrs['type'],
+      'name' => $attrs['name'],
+      'value'=> $attrs['value'],
+      'min' => $attrs['min'],
+      'max' => $attrs['max']
+    ];
+  }
+
+  public function get_inputs(){
+    return $this->inputs;
+  }
+
+  public function calculate(){}
+}
