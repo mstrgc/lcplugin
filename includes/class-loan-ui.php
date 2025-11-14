8d11f8c11d17edfa8aee6cedb25c7dd49835d391
mstrgc <gcmstr@gmail.com>
2025-11-14 12:36:02 +0330

    add create input method

--- includes/class-loan-ui.php
+++ includes/class-loan-ui.php
@@ -6,10 +6,19 @@ if(!defined('ABSPATH')) {
 
 class Loan_Calc_UI{
   public function __construct($input){
-    $this->render_ui($input);
+    $this->render_ui();
   }
 
-  public function render_ui($input){
-    echo $input;
+  public function create_input($attrs){
+    $this->inputs[] = [
+      'type' => $attrs['type'],
+      'name' => $attrs['name']
+    ];
+  }
+
+  private $inputs =[];
+
+  public function render_ui(){
+    echo '';
   }
 }
