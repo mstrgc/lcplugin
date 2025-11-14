1083dd816062af20145963431c79168a7a66dfd3
mstrgc <gcmstr@gmail.com>
2025-11-14 11:37:55 +0330

    add loan loader php file

---
+++ includes/lass-loan-loader.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!defined('ABSPATH')) {
+  exit;
+}
+
+class Loan_Calc_Loader{
+  public function __construct($bank){
+    $this->bank = $bank;
+  }
+
+  public function load_ui(){
+
+  }
+}
