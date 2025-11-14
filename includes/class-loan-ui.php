d9e232cf7a40d42ef9fdc443ef3940b42be5f144
mstrgc <gcmstr@gmail.com>
2025-11-14 17:09:27 +0330

    add radio input displayer

--- includes/class-loan-ui.php
+++ includes/class-loan-ui.php
@@ -10,12 +10,19 @@ class Loan_Calc_UI{
   public function render_ui($inputs){
     error_log(count($inputs));
     foreach($inputs as $input){
-      echo "<label for='{$input['name']}'>{$input['name']}</label>";
       if ($input['type'] == 'range') $this->render_range_input($input);
+      if ($input['type'] == 'radio') $this->render_radio_input($input);
     }
   }
 
   private function render_range_input($attrs){
+    echo "<label for='{$attrs['name']}'>{$attrs['name']}</label>";
     echo "<input type='range' name='{$attrs['name']}' id='{$attrs['name']}' min='{$attrs['min']}' max='{$attrs['max']}' step='{$attrs['step']}'>";
+  }
+
+  private function render_radio_input($attrs){
+    foreach($attrs['options'] as $option){
+      echo "<label for='{$attrs['name']}'><input type='radio' name='{$attrs['name']}' id='{$attrs['name']}' value='{$option}'>$option</label>";
+    }
   }
 }
