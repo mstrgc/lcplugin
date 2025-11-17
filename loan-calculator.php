48372b2f3520ccd01934f0fa0291f7265463f86e
mstrgc <gcmstr@gmail.com>
2025-11-17 11:45:48 +0330

    add core folder

--- loan-calculator.php
+++ loan-calculator.php
@@ -8,10 +8,12 @@ if(!defined('ABSPATH')) {
 	exit;
 }
 
+require __DIR__ . '/vendor/autoload.php';
+
+use App\Core\Plugin;
+
+new Plugin();
+
 define('LCPLUGIN_PATH', plugin_dir_path(__FILE__));
 define('LCPLUGIN_INCLUDES', LCPLUGIN_PATH . 'includes/');
 define('LCPLUGIN_CONFIG', LCPLUGIN_PATH . 'config/');
-
-require_once LCPLUGIN_INCLUDES . 'class-loan-shortcode.php';
-
-new Loan_Calc_Shortcode();
