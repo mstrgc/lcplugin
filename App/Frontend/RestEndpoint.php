7bb76752f9f8fcd2b2e03a0d5e52f3b41cfb5af1
mstrgc <gcmstr@gmail.com>
2025-11-17 15:37:49 +0330

    add test rest api

--- App/Frontend/RestEndpoint.php
+++ App/Frontend/RestEndpoint.php
@@ -9,5 +9,26 @@ if(!defined('ABSPATH')) {
 }
 
 class RestEndpoint{
-  
+  public function register() {
+    add_action('rest_api_init', function () {
+      register_rest_route(
+        'loan-calculator/v1',
+        '/calculate',
+        [
+          'methods'  => 'POST',
+          'callback' => [$this, 'calc'],
+          'permission_callback' => '__return_true',
+        ]
+      );
+    });
+  }
+
+  public function calc(\WP_REST_Request $request){
+    // Get form data
+    $price = $request->get_param('price');
+
+    return [
+      'amount' => $price,
+    ];
+  }
 }
