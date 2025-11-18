265d95675d73960bb716dfde6050d70319c10ddf
mstrgc <gcmstr@gmail.com>
2025-11-18 13:10:06 +0330

    add active config param

--- App/Core/Plugin.php
+++ App/Core/Plugin.php
@@ -12,10 +12,15 @@ if(!defined('ABSPATH')) {
 class Plugin{
 
   protected $active_config = null;
-  
+
   public function __construct(){
     new Shortcode();
     $rest_api = new RestEndpoint();
     $rest_api->register();
+  }
+
+  public function set_active_config($config){
+    $config_name = '\Config\\' . ucfirst($config);
+    $this-> active_config = $config_name;
   }
 }
