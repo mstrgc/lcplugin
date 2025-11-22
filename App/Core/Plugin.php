bef0aad834a1c7412c4ed768c300647665922ee7
mstrgc <gcmstr@gmail.com>
2025-11-22 12:52:10 +0330

    add singleton to core pluin file

--- App/Core/Plugin.php
+++ App/Core/Plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Core;
+//namespace App\Core;
 
 use App\Frontend\Shortcode;
 use App\Frontend\RestEndpoint;
@@ -10,12 +10,22 @@ if(!defined('ABSPATH')) {
 }
 
 class Plugin{
+
+  public static $instance;
+
   public function __construct(){
     $shortcode = Shortcode::get_instance();
     add_shortcode('loan-calculator', [$shortcode, 'render_shortcode']);
     $rest_api = new RestEndpoint();
     $rest_api->register();
   }
+
+  public static function get_instance() {
+    if(is_null(self::$instance)){
+      self::$instance = new self();
+    }
+    return self::$instance;
+  }
 }
 
-add_action('plugins_loaded', fn() => new Plugin());
+add_action('plugins_loaded', ['Plugin', 'get_instance']);
